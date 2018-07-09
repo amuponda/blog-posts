7e727e8c3bf5eae2bd4bc2bf4968421728911b3c
Alfred Muponda <alfredmuponda@gmail.com>
2018-07-10 04:54:51 +1000

    MDBootstrap Vee-Validate integration blog

---
+++ mdbbootstrap-vee-validate/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <container>
+    <form @submit.prevent="login">
+      <p class="h4 text-center mb-4">Sign in</p>
+      <div class="grey-text">
+        <md-input
+          v-validate="'required|email'"
+          name="email"
+          label="Email"
+          icon="envelope"
+          type="text"
+          :value="email"
+          @input="email = $event">
+          <p v-show="errors.has('email')" class="text-danger"><small>{{ errors.first('email') }}</small></p>
+        </md-input>
+      </div>
+      <button type="submit">Test</button>
+    </form>
+  </container>
+</template>
+
+<script>
+import MdInput from 'mdbvue/src/components/MdInput'
+import Container from 'mdbvue/src/components/Container'
+
+export default {
+  name: 'HelloWorld',
+  components: {MdInput, Container},
+  data () {
+    return {
+      email: ''
+    }
+  },
+  methods: {
+    login () {
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          alert("Email is: " + this.email)
+        }
+      })
+    }
+  }
+}
+</script>
+
+
+</style>
---
+++ mdbbootstrap-vee-validate/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VeeValidate from 'vee-validate'
+import './assets/css/bootstrap.css'
+import './assets/css/mdb.css'
+
+Vue.config.productionTip = false
+
+Vue.use(VeeValidate)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
