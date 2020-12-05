95b3f91979718898f6a2fe7390f55f13950455c5
Alfred Muponda <alfredmuponda@gmail.com>
2020-12-05 16:37:45 +1000

    Add the source code for the micronaut-vue spa blogpost

---
+++ mn-spa-vue/server/src/main/groovy/mn/spa/vue/Application.groovy
@@ -0,0 +1,11 @@
+package mn.spa.vue
+
+import io.micronaut.runtime.Micronaut
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Application {
+    static void main(String[] args) {
+        Micronaut.run(Application, args)
+    }
+}
---
+++ mn-spa-vue/server/src/test/groovy/mn/spa/vue/ServerSpec.groovy
@@ -0,0 +1,19 @@
+package mn.spa.vue
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class ServerSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
